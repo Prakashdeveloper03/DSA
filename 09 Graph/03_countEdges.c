72353b4c6ad7d5ebc6e97eb79bd3c98ba46e469b
Siva Prakash <prakashdeveloper@outlook.in>
2023-01-04 08:26:39 +0530

    Added `09 Graph` folder

---
+++ 09 Graph/03_countEdges.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int adj[100][100];
+int n;
+int main()
+{
+    int maxe, i, j, org, des = 0, count = 0;
+    printf("Enter vertices of graph : ");
+    scanf("%d", &n);
+    maxe = n * (n - 1);
+    for (i = 1; i <= maxe; i++)
+    {
+        printf("Enter edge of vertices %d \nPress -1 for Stop\n", i);
+        scanf("%d %d", &org, &des);
+        if ((org == -1) && (des == -1))
+            break;
+
+        if (org >= n || des >= n || org < 0 || des < 0)
+        {
+            printf("Invalid Input\n");
+            i--;
+        }
+        else
+        {
+            adj[org][des] = 1;
+            count++;
+        }
+    }
+    printf("Adjancy Matrix is \n");
+    for (i = 0; i < n; i++)
+        printf("%4d", i);
+    printf("\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", i);
+        for (j = 0; j < n; j++)
+        {
+            printf("%4d", adj[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Total Edge is %d\n", count);
+    return 0;
+}
---
+++ 09 Graph/readme.md
@@ -0,0 +1,11 @@
+# 09 Graph
+
+1.	Develop and Implement a Program for the following operations on Graph(G).<br>
+    a. Create a Graph of N vertex using Adjacency Matrix.<br>
+    b. Print all the nodes reachable from a given starting node in a digraph using DFS method
+2.	Develop and Implement a Program for the following operations on Graph(G).<br>
+    a. Create a Graph of N vertex using Adjacency Matrix.<br>
+    b. Print all the nodes reachable from a given starting node in a digraph using BFS method
+3.	Write a program to count the number of edges in the graph.
+4.	Write a program to obtain the topological ordering of vertices in a digraph.
+5.	Write a program to find the degree of each vertex In a graph.
